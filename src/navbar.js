cac742ead8b49a8c6501f59c5b45738a88eca837
jimmybwheeler <jimmybwheeler@gmail.com>
2024-03-24 13:33:56 -0700

    started on nav bar items

--- src/navbar.js
+++ src/navbar.js
@@ -1,1 +1,13 @@
 const navBar = document.querySelector('.navBar');
+
+function addNavBarMenus(){
+    navBar.append(navBarMenu())
+}
+
+function navBarMenu(){
+    const div = document.createElement('div');
+    div.innerText = 'Home';
+    return div;
+}
+
+addNavBarMenus();
