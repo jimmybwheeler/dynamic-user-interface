52d565830c2156b2d6d04d07df174ba5df3b8bcb
jimmy <jimmybwheeler@gmail.com>
2024-03-25 15:15:52 -0700

    removed some js code and hard coded nav items, also changed size of divs in css

--- src/navbar.js
+++ src/navbar.js
@@ -1,13 +1,1 @@
 const navBar = document.querySelector(".navBar");
-
-function addNavBarMenus() {
-  navBar.append(navBarMenu());
-}
-
-function navBarMenu() {
-  const div = document.createElement("div");
-  div.innerText = "Home";
-  return div;
-}
-
-addNavBarMenus();
