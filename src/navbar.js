c0a924f0507332d726a77552889865ce9d0e0d1d
jimmy <jimmybwheeler@gmail.com>
2024-03-25 15:56:42 -0700

    added mouseover event to first menu to show hidden list

--- src/navbar.js
+++ src/navbar.js
@@ -1,1 +1,10 @@
 const navBar = document.querySelector(".navBar");
+const menuOne = document.querySelector('.menuOne');
+const menuTwo = document.querySelector('.menuTwo')
+const menuThree = document.querySelector('.menuThree')
+
+menuOne.addEventListener('mouseover', showDropDown);
+
+function showDropDown(){
+  document.querySelector('.menuOneDropDown').classList.add('showDropDownMenu');
+}
