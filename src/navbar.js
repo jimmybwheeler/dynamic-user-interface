c8ac502b0ab7d9ccbaea2e3537050dffa3152142
jimmy <jimmybwheeler@gmail.com>
2024-03-26 11:45:22 -0700

    added mouseout even that needs fixing, added css to drop down menu:

--- src/navbar.js
+++ src/navbar.js
@@ -4,7 +4,12 @@ const menuTwo = document.querySelector('.menuTwo')
 const menuThree = document.querySelector('.menuThree')
 
 menuOne.addEventListener('mouseover', showDropDown);
+menuOne.addEventListener('mouseout', hideDropDown)
 
 function showDropDown(){
   document.querySelector('.menuOneDropDown').classList.add('showDropDownMenu');
+}
+
+function hideDropDown(){
+  document.querySelector('.menuOneDropDown').classList.remove('showDropDownMenu');
 }
