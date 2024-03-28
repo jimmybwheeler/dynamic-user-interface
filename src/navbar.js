ceabccaf48e61ecf8dfb32b7ecb3dce7078dc658
jimmybwheeler <jimmybwheeler@gmail.com>
2024-03-27 23:02:12 -0700

    used prettier

--- src/navbar.js
+++ src/navbar.js
@@ -1,15 +1,17 @@
 const navBar = document.querySelector(".navBar");
-const menuOne = document.querySelector('.menuOne');
-const menuTwo = document.querySelector('.menuTwo')
-const menuThree = document.querySelector('.menuThree')
+const menuOne = document.querySelector(".menuOne");
+const menuTwo = document.querySelector(".menuTwo");
+const menuThree = document.querySelector(".menuThree");
 
-menuOne.addEventListener('mouseover', showDropDown);
-menuOne.addEventListener('mouseout', hideDropDown)
+menuOne.addEventListener("mouseover", showDropDown);
+menuOne.addEventListener("mouseout", hideDropDown);
 
-function showDropDown(){
-  document.querySelector('.menuOneDropDown').classList.add('showDropDownMenu');
+function showDropDown() {
+  document.querySelector(".menuOneDropDown").classList.add("showDropDownMenu");
 }
 
-function hideDropDown(){
-  document.querySelector('.menuOneDropDown').classList.remove('showDropDownMenu');
+function hideDropDown() {
+  document
+    .querySelector(".menuOneDropDown")
+    .classList.remove("showDropDownMenu");
 }
