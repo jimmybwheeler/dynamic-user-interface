57fe831daf0f83ba24e300980fb6291e5d1bf781
jimmybwheeler <jimmybwheeler@gmail.com>
2024-03-30 14:02:13 -0700

    removed code

--- src/navbar.js
+++ src/navbar.js
@@ -4,14 +4,9 @@ const menuTwo = document.querySelector(".menuTwo");
 const menuThree = document.querySelector(".menuThree");
 
 menuOne.addEventListener("mouseover", showDropDown);
-menuOne.addEventListener("mouseout", hideDropDown);
 
 function showDropDown() {
-  document.querySelector(".menuOneDropDown").classList.add("showDropDownMenu");
-}
-
-function hideDropDown() {
-  document
-    .querySelector(".menuOneDropDown")
-    .classList.remove("showDropDownMenu");
+  const dropDown = document.querySelector('.dropDown');
+  dropDown.style.display = 'none';
+  dropDown.style.display == 'none' ? 'block' : 'none'
 }
