fe1e78b99cdc8b90c43000efb62fd0763de7b744
jimmy <jimmybwheeler@gmail.com>
2024-04-03 14:54:18 -0700

    changed background color on item 1 click and used prettier

--- src/navbar.js
+++ src/navbar.js
@@ -1,14 +1,15 @@
-const menuOne = document.querySelector('.menuOne');
-menuOne.addEventListener('click', showDropDown)
+const menuOne = document.querySelector(".menuOne");
+menuOne.addEventListener("click", showDropDown);
 
-const dropDownItemOne = document.querySelector('.dropDownItemOne')
-dropDownItemOne.addEventListener('click', showItemOne)
+const dropDownItemOne = document.querySelector(".dropDownItemOne");
+dropDownItemOne.addEventListener("click", showItemOne);
 
 function showDropDown() {
-    const dropDown = document.querySelector('.dropDown')
-    dropDown.classList.toggle('showDropDown');
+  const dropDown = document.querySelector(".dropDown");
+  dropDown.classList.toggle("showDropDown");
 }
 
 function showItemOne() {
-    console.log('test')
+  console.log("test");
+  document.body.style.backgroundColor = "red";
 }
