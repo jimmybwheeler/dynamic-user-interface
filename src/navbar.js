6749907796c8c7e6c28291875d286f53cecf09bc
jimmybwheeler <jimmybwheeler@gmail.com>
2024-04-04 13:36:09 -0700

    added a value sent to change what items appear in drop down when clicked

--- src/navbar.js
+++ src/navbar.js
@@ -1,20 +1,33 @@
 const tShirtMenu = document.querySelector(".tShirtMenu");
-tShirtMenu.addEventListener("click", showDropDown);
+tShirtMenu.addEventListener("click", function(){
+    showDropDown('shirts');
+});
 
 const pantsMenu = document.querySelector('.pantsMenu')
-pantsMenu.addEventListener("click", showDropDown);
+pantsMenu.addEventListener("click", function(){
+    showDropDown('pants');
+});
 
 const jacketMenu = document.querySelector('.jacketMenu')
-jacketMenu.addEventListener("click", showDropDown);
+jacketMenu.addEventListener("click", function(){
+    showDropDown('jackets');
+});
 
 const dropDownItems = document.querySelectorAll('.dropDownItem');
 dropDownItems.forEach((item) => {
     item.addEventListener('click', changeBackgroundColor)
 })
 
-function showDropDown() {
+function showDropDown(clothingType) {
   const dropDown = document.querySelector(".dropDown");
   dropDown.classList.toggle("showDropDown");
+  if(clothingType == 'shirts'){
+    console.log('shirts')
+  } else if (clothingType == 'pants'){
+    console.log('pants')
+  } else if (clothingType == 'jackets'){
+    console.log('jackets')
+  }
 }
 
 function changeBackgroundColor() {
