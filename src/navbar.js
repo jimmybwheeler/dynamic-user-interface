8a9f2f7a2bbdcf725804ad6b03885d1d66c7d9d9
jimmybwheeler <jimmybwheeler@gmail.com>
2024-04-06 13:10:05 -0700

    used prettier

--- src/navbar.js
+++ src/navbar.js
@@ -1,45 +1,45 @@
 const tShirtMenu = document.querySelector(".tShirtMenu");
-tShirtMenu.addEventListener("click", function(){
-    showDropDown('shirts');
+tShirtMenu.addEventListener("click", function () {
+  showDropDown("shirts");
 });
 
-const pantsMenu = document.querySelector('.pantsMenu')
-pantsMenu.addEventListener("click", function(){
-    showDropDown('pants');
+const pantsMenu = document.querySelector(".pantsMenu");
+pantsMenu.addEventListener("click", function () {
+  showDropDown("pants");
 });
 
-const jacketMenu = document.querySelector('.jacketMenu')
-jacketMenu.addEventListener("click", function(){
-    showDropDown('jackets');
+const jacketMenu = document.querySelector(".jacketMenu");
+jacketMenu.addEventListener("click", function () {
+  showDropDown("jackets");
 });
 
-const dropDownItems = document.querySelectorAll('.dropDownItem');
+const dropDownItems = document.querySelectorAll(".dropDownItem");
 dropDownItems.forEach((item) => {
-    item.addEventListener('click', changeBackgroundColor)
-})
+  item.addEventListener("click", changeBackgroundColor);
+});
 
 function showDropDown(clothingType) {
-  const dropDownItems = document.querySelectorAll('.dropDownItem')
+  const dropDownItems = document.querySelectorAll(".dropDownItem");
   const dropDown = document.querySelector(".dropDown");
   dropDown.classList.add("showDropDown");
-  if(clothingType == 'shirts'){
-    console.log('shirts')
-    let arr = ['Shirt 1', 'Shirt 2', 'Shirt 3']
-  } else if (clothingType == 'pants'){
-    console.log('pants')
-    let arr = ['Pants 1', 'Pants 2', 'Pants 3']
-  } else if (clothingType == 'jackets'){
-    console.log('jackets')
-    let arr = ['Jacket 1', 'Jacket 2', 'Jacket 3']
+  if (clothingType == "shirts") {
+    console.log("shirts");
+    let arr = ["Shirt 1", "Shirt 2", "Shirt 3"];
+  } else if (clothingType == "pants") {
+    console.log("pants");
+    let arr = ["Pants 1", "Pants 2", "Pants 3"];
+  } else if (clothingType == "jackets") {
+    console.log("jackets");
+    let arr = ["Jacket 1", "Jacket 2", "Jacket 3"];
   }
 }
 
 function changeBackgroundColor() {
-    document.body.style.backgroundColor = `${randomColor()}`;
-    document.querySelector('.dropDown').classList.toggle('showDropDown')
+  document.body.style.backgroundColor = `${randomColor()}`;
+  document.querySelector(".dropDown").classList.toggle("showDropDown");
 }
 
 function randomColor() {
-    let arr = ['red', 'green', 'blue']
-    return arr[Math.floor(Math.random() * 3)];
+  let arr = ["red", "green", "blue"];
+  return arr[Math.floor(Math.random() * 3)];
 }
